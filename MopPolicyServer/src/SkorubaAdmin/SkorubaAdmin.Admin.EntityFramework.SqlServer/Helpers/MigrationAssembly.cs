bb7b6e0a1e25267fbc6dbfadfa530e252e778c5e
GrzegorzStąpała <grzegorz.stapala@gmail.com>
2023-12-20 12:16:19 +0100

    Adding Skoruba Identity Server to solution

---
+++ MopPolicyServer/src/SkorubaAdmin/SkorubaAdmin.Admin.EntityFramework.SqlServer/Helpers/MigrationAssembly.cs
@@ -0,0 +1,14 @@
+﻿namespace SkorubaAdmin.Admin.EntityFramework.SqlServer.Helpers
+{
+    public class MigrationAssembly
+    {
+        
+    }
+}
+
+
+
+
+
+
+
---
+++ MopPolicyServer/src/SkorubaAdmin/SkorubaAdmin.STS.Identity/ViewModels/Manage/ShowRecoveryCodesViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SkorubaAdmin.STS.Identity.ViewModels.Manage
+{
+    public class ShowRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
